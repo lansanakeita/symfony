f65b042db2c03b2fc9ff7cf99a668e1091d88cc6
242darkman <bvouvou@gmail.com>
2023-01-31 09:31:48 +0100

    feature: ajout des entités Atelier, Salle, Participation, Secteur

---
+++ assets/vue/controllers/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div> Hello forum {{ nom }} </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  props: {
+    nom: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Entity/Salle.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SalleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SalleRepository::class)]
+class Salle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom_salle = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $etage = null;
+
+    #[ORM\Column]
+    private ?int $capacite_max = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    public function getNomSalle(): ?string
+    {
+        return $this->nom_salle;
+    }
+
+    public function setNomSalle(string $nom_salle): self
+    {
+        $this->nom_salle = $nom_salle;
+
+        return $this;
+    }
+
+    public function getEtage(): ?string
+    {
+        return $this->etage;
+    }
+
+    public function setEtage(string $etage): self
+    {
+        $this->etage = $etage;
+
+        return $this;
+    }
+
+    public function getCapaciteMax(): ?int
+    {
+        return $this->capacite_max;
+    }
+
+    public function setCapaciteMax(int $capacite_max): self
+    {
+        $this->capacite_max = $capacite_max;
+
+        return $this;
+    }
+}
