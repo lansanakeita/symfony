bb124bddb3113a158f8607f22de8adc370b2c575
242darkman <bvouvou@gmail.com>
2023-01-31 10:34:41 +0100

    fix: modification de l'entité salle

--- src/Entity/Salle.php
+++ src/Entity/Salle.php
@@ -13,5 +13,50 @@ class Salle
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 100)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $etage = null;
+
+    #[ORM\Column]
+    private ?int $capicite = null;
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getEtage(): ?string
+    {
+        return $this->etage;
+    }
+
+    public function setEtage(string $etage): self
+    {
+        $this->etage = $etage;
+
+        return $this;
+    }
+
+    public function getCapicite(): ?int
+    {
+        return $this->capicite;
+    }
+
+    public function setCapicite(int $capicite): self
+    {
+        $this->capicite = $capicite;
+
+        return $this;
+    }
+
 
 }
